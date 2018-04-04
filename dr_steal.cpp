9eafe54ecf772171baddd3ed4de9d5623199919e
root <rahulrane50@gmail.com>
2018-04-04 15:24:47 -0400

    Added sync_cnt_locks.

--- dr_steal.cpp
+++ dr_steal.cpp
@@ -5,14 +5,22 @@
 #include <map>
 using namespace std;
 
-int matrix_sz=32;
+/*int matrix_sz=32;
 vector<vector<int> > x(matrix_sz, vector<int>(matrix_sz, 3));
 vector<vector<int> > y(matrix_sz, vector<int>(matrix_sz, 3));
-vector<vector<int> > z(matrix_sz, vector<int>(matrix_sz));
+vector<vector<int> > z(matrix_sz, vector<int>(matrix_sz));*/
+#define N 32
+int m=2;
+
+int x[N][N];
+int y[N][N];
+int z[N][N];
+
 
 map<pthread_t, deque<function<void()>>> que_map;
 map<pthread_t, pthread_cond_t> que_cond_map;
 map<pthread_t, pthread_mutex_t> lock_map;
+pthread_mutex_t sync_cnt_lock;
 
 bool completed=false;
 
@@ -22,25 +30,47 @@ void submit_task(PTHREADID tid, CALLABLE fn, ARGS&&... args ) {
     que_map[tid].push_back( bind( fn, args... ) ) ; 
 }
 
-void print_matrix(vector<vector<int> > a) {
+void populate_matrix(int a[N][N]) {
+    for(int i=0;i<N;i++) {
+        for(int j=0; j<N;j++) {
+            a[i][j] = 3;
+        }
+    }
+}
+
+void print_matrix(int a[N][N]) {
     cout<<"Matrix :"<<endl;
-    for(int i=0;i<a.size();i++) {
-        for(int j=0; j<a[0].size();j++) {
+    for(int i=0;i<N;i++) {
+        for(int j=0; j<N;j++) {
             cout<<a[i][j]<<" ";
         }
         cout<<"\n";
     }
 }
 
+void serial_mm(int r_z, int c_z, int x[N][N], int r_x, int c_x, int y[N][N], int r_y, int c_y, int m) {
 
-void parallel_rec_mm(int r_z, int c_z, vector<vector<int> > x, int r_x, int c_x, vector<vector<int> > y, int r_y, int c_y, int n, int *parent_sync_cnt, int *child_sync_cnt, int *rp) {
+        for(int i=r_x, u=r_z; i<r_x+m; i++,u++) {
+                for(int k=c_x; k<c_x+m; k++) {
+                        for(int j=c_y,v=c_z ; j<c_y+m; j++,v++) {
+                                z[u][v] = z[u][v] + x[i][k] * y[k][j];
+                        }
+                }
+        }
+} 
+
+void parallel_rec_mm(int r_z, int c_z, int x[N][N], int r_x, int c_x, int y[N][N], int r_y, int c_y, int n, int *parent_sync_cnt, int *child_sync_cnt, int *rp) {
     pthread_t tid = pthread_self();
     cout<<"parallel_rec_mm: n="<<n<<endl;
     if (n == 1) {
         z[r_z][c_z] = z[r_z][c_z] + x[r_x][c_x]*y[r_y][c_y];
+	    pthread_mutex_lock(&sync_cnt_lock);
         (*parent_sync_cnt)--;
+	    pthread_mutex_unlock(&sync_cnt_lock);
     } else {
+	    pthread_mutex_lock(&sync_cnt_lock);
         if (*rp == 0) {
+            pthread_mutex_unlock(&sync_cnt_lock);
             int *new_child_sync_cnt1 = new int(4);
             int *child_rp1 = new int(0);
             submit_task(tid, parallel_rec_mm, r_z, c_z, x, r_x, c_x, y, r_y, c_y, n/2, child_sync_cnt, new_child_sync_cnt1, child_rp1);
@@ -56,22 +86,35 @@ void parallel_rec_mm(int r_z, int c_z, vector<vector<int> > x, int r_x, int c_x,
             int *child_rp4 = new int(0);
             submit_task(tid, parallel_rec_mm, r_z+n/2, c_z+n/2, x, r_x+n/2, c_x, y, r_y, c_y+n/2, n/2, child_sync_cnt, new_child_sync_cnt4, child_rp4);
             
+	        pthread_mutex_lock(&sync_cnt_lock);
             if (*(child_sync_cnt) > 0) {
                 *rp = 1;
                 submit_task(tid, parallel_rec_mm, r_z, c_z, x, r_x, c_x, y, r_y, c_y, n, parent_sync_cnt, child_sync_cnt, rp);
+	            pthread_mutex_unlock(&sync_cnt_lock);
                 return;
             }
+	        pthread_mutex_unlock(&sync_cnt_lock);
+        } else {
+            pthread_mutex_unlock(&sync_cnt_lock);
         }
+	    
+        pthread_mutex_lock(&sync_cnt_lock);
         if (*rp == 1) {
             if (*(child_sync_cnt) > 0) {
                 *rp = 1;
                 submit_task(tid, parallel_rec_mm, r_z, c_z, x, r_x, c_x, y, r_y, c_y, n, parent_sync_cnt, child_sync_cnt, rp);
+	            pthread_mutex_unlock(&sync_cnt_lock);
                 return;
             }
-        } 
+	        pthread_mutex_unlock(&sync_cnt_lock);
+        } else {
+	        pthread_mutex_unlock(&sync_cnt_lock);
+        }
         
+        pthread_mutex_lock(&sync_cnt_lock);
         if (*rp != 2) {
             *child_sync_cnt = 4;
+	        pthread_mutex_unlock(&sync_cnt_lock);
             int *new_child_sync_cnt5 = new int(4);
             int *child_rp5 = new int(0);
             submit_task(tid, parallel_rec_mm, r_z, c_z, x, r_x, c_x+n/2, y, r_y+n/2, c_y, n/2, child_sync_cnt, new_child_sync_cnt5, child_rp5);
@@ -87,13 +130,19 @@ void parallel_rec_mm(int r_z, int c_z, vector<vector<int> > x, int r_x, int c_x,
             int *new_child_sync_cnt8 = new int(4);
             int *child_rp8 = new int(0);
             submit_task(tid, parallel_rec_mm, r_z+n/2, c_z+n/2, x, r_x+n/2, c_x+n/2, y, r_y+n/2, c_y+n/2, n/2, child_sync_cnt, new_child_sync_cnt8, child_rp8);
+        } else {
+	        pthread_mutex_unlock(&sync_cnt_lock);
         }
+	    
+        pthread_mutex_lock(&sync_cnt_lock);
         if (*(child_sync_cnt) > 0) {
             *rp = 2;
             submit_task(tid, parallel_rec_mm, r_z, c_z, x, r_x, c_x, y, r_y, c_y, n, parent_sync_cnt, child_sync_cnt, rp);
+	        pthread_mutex_unlock(&sync_cnt_lock);
             return;
         } else {
             (*parent_sync_cnt)--;
+	        pthread_mutex_unlock(&sync_cnt_lock);
         }
     }
 }
@@ -170,7 +219,7 @@ void dr_steal() {
     int *rp = new int(0);
     int *child_sync_cnt = new int(4);
  
-    submit_task(tids[0], parallel_rec_mm, 0, 0, x, 0, 0, y, 0, 0, matrix_sz, parent_sync_cnt, child_sync_cnt, rp);
+    submit_task(tids[0], parallel_rec_mm, 0, 0, x, 0, 0, y, 0, 0, N, parent_sync_cnt, child_sync_cnt, rp);
 
     while(*rp != 2);
     completed = true;
@@ -188,9 +237,9 @@ int main(int argc, char *argv[]) {
     //vector<vector<int> > y(n, vector<int>(n, 3));
     //vector<vector<int> > z(n, vector<int>(n));
 
-    //populate_matrix(x);
-    //populate_matrix(y);
-    //populate_matrix(x);
+    populate_matrix(x);
+    populate_matrix(y);
+    populate_matrix(x);
     print_matrix(x);
     print_matrix(y);
     print_matrix(z);
